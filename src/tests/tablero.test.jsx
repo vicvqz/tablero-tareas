9afbec29d2a5a47b0816eb0db2e6ca8bdc231e79
vicvqz <victoriavqz.2025@gmail.com>
2026-06-12 20:47:33 -0300

    agrego una prueba para verificar que no se añada una tarea vacia

--- src/tests/tablero.test.jsx
+++ src/tests/tablero.test.jsx
@@ -38,3 +38,17 @@ test("adds a task", async () => {
     screen.getByText("Preparar presentación")
   ).toBeInTheDocument();
 });
+
+test("does not add empty task", async () => {
+  render(<Tablero />);
+
+  await userEvent.click(
+    screen.getByRole("button", {
+      name: /agregar/i,
+    })
+  );
+
+  expect(
+    screen.queryByText("")
+  ).not.toBeInTheDocument();
+});

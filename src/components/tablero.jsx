71ca19e5f463d396c9c9fcc6baa1abb35e8591ad
vicvqz <victoriavqz.2025@gmail.com>
2026-06-12 17:04:04 -0300

    modificacion visualizacion columnas

--- src/components/tablero.jsx
+++ src/components/tablero.jsx
@@ -40,22 +40,37 @@ export default function Tablero() {
         Agregar
       </button>
 
-      <div>
-        <h2>Por hacer</h2>
-        {tasks
-          .filter(task => task.status === "todo")
-          .map(task => (
-            <p key={task.id}>{task.title}</p>
-          ))}
-      </div>
+ <div className="board">
+  <div className="column">
+    <h2>Por hacer</h2>
 
-      <div>
-        <h2>Haciendo</h2>
-      </div>
+    {tasks
+      .filter(task => task.status === "Por hacer")
+      .map(task => (
+        <p key={task.id}>{task.title}</p>
+      ))}
+  </div>
 
-      <div>
-        <h2>Hecho</h2>
-      </div>
+<div className="column">
+    <h2>Haciendo</h2>
+
+    {tasks
+      .filter(task => task.status === "Haciendo")
+      .map(task => (
+        <p key={task.id}>{task.title}</p>
+      ))}
+  </div>
+
+  <div className="column">
+    <h2>Hecho</h2>
+
+    {tasks
+      .filter(task => task.status === "Hecho")
+      .map(task => (
+        <p key={task.id}>{task.title}</p>
+      ))}
+  </div>
+</div>
     </>
   );
 }
